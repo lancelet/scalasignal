f720977d3f721f32d7d3ec50ddb69fc1233f04ef
Jonathan Merritt <merritt@unimelb.edu.au>
2011-05-13 21:53:53 -0400

    Added Scalala and a commented unchecked warnings option.

--- project/build/ScalaSignalProject.scala
+++ project/build/ScalaSignalProject.scala
@@ -2,7 +2,16 @@ import sbt._
 
 class ScalaSignalProject(info: ProjectInfo) extends DefaultProject(info) {
 
-  val scalaTest = "org.scalatest" % "scalatest_2.9.0" % "1.4.1"
+  val scalaToolsSnapshots = "Scala-Tools Snapshots" at "http://scala-tools.org/repo-snapshots"
+
+  // ScalaLA stuff
+  val scalaNLPRepo = "ScalaNLP" at "http://repo.scalanlp.org/repo"
+  val ondexRepo = "ondex" at "http://ondex.rothamsted.bbsrc.ac.uk/nexus/content/groups/public"
+  val scalala = "org.scalala" %%  "scalala" % "1.0.0.RC2-SNAPSHOT"
+
+  val scalaTest = "org.scalatest" %% "scalatest" % "1.4.1"
   // val scalaCheck = "org.scala-tools.testing" % "scalacheck_2.8.1" % "1.8"
 
+  // enable unchecked warnings
+  //override def compileOptions = super.compileOptions ++ Seq(Unchecked)
 }
