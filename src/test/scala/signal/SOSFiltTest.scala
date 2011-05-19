99862747a88730d8abb3cf401815a16a1803e0e6
Jonathan Merritt <merritt@unimelb.edu.au>
2011-05-18 21:27:08 -0400

    Added 4th order Butterworth filter test to SOSFiltTest.

--- src/test/scala/signal/SOSFiltTest.scala
+++ src/test/scala/signal/SOSFiltTest.scala
@@ -13,7 +13,20 @@ class SOSFiltTest extends FunSuite {
     assert(doubleItAeq(result, expected))
   }
 
-  test("apply a 4-th order Butterworth filter with an SOSFilt stack") (pending)
+  test("apply a 4-th order Butterworth filter with an SOSFilt stack") {
+    // generate the filters using butterSOSEven
+    val fStack = Butter.butterSOSEven(4, 0.2)
+    // test data
+    val x = List(1., 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    // expected output (generated using Octave)
+    val yExpected = List(0.0048243, 0.0403774, 0.1665251, 0.4606177, 
+			 0.9793515, 1.7315426, 2.6772461, 3.7467150,
+			 4.8657874, 5.9763508)
+    val y = SOSFilt.sosfilt(fStack, x)
+    assert(doubleItAeq(y, yExpected, 1e-7))
+  }
+
+  test("apply a 4-th order Butterworth filter specified using a Matrix") (pending)
 
   test("SOSFilt evaluation should be lazy where possible") (pending)
 
