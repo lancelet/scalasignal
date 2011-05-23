88fd060f2b9215b1dc32208e73df8bdcc2ec259e
Jonathan Merritt <merritt@unimelb.edu.au>
2011-05-23 17:54:49 -0400

    Added Apache Commons Math as a dependency.

--- project/build/ScalaSignalProject.scala
+++ project/build/ScalaSignalProject.scala
@@ -4,15 +4,20 @@ import de.element34.sbteclipsify._
 class ScalaSignalProject(info: ProjectInfo) extends DefaultProject(info) 
 with Eclipsify {
 
-  val scalaToolsSnapshots = "Scala-Tools Snapshots" at "http://scala-tools.org/repo-snapshots"
+  val scalaToolsSnapshots = "Scala-Tools Snapshots" at 
+    "http://scala-tools.org/repo-snapshots"
 
   // ScalaLA stuff
   val scalaNLPRepo = "ScalaNLP" at "http://repo.scalanlp.org/repo"
-  val ondexRepo = "ondex" at "http://ondex.rothamsted.bbsrc.ac.uk/nexus/content/groups/public"
+  val ondexRepo = "ondex" at 
+    "http://ondex.rothamsted.bbsrc.ac.uk/nexus/content/groups/public"
   val scalala = "org.scalala" %%  "scalala" % "1.0.0.RC2-SNAPSHOT"
 
   val scalaTest = "org.scalatest" %% "scalatest" % "1.4.1"
   // val scalaCheck = "org.scala-tools.testing" % "scalacheck_2.8.1" % "1.8"
+
+  // Apache Commons Math
+  val commonsMath = "org.apache.commons" % "commons-math" % "2.0"
 
   // enable unchecked warnings
   //override def compileOptions = super.compileOptions ++ Seq(Unchecked)
