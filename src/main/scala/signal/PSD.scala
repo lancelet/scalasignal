7a13f13d94cfcc44b5aed876c3b079db6435b427
Jonathan Merritt <merritt@unimelb.edu.au>
2011-05-29 13:31:28 -0400

    Added brief comment about PSD TODO.

--- src/main/scala/signal/PSD.scala
+++ src/main/scala/signal/PSD.scala
@@ -10,26 +10,28 @@ object PSD {
    *  The PSD is estimated using a Fast Fourier Transform.  The sequence of
    *  steps is as follows:
    *  
-   *   1. The signal in `x` is padded to the next closest power of 2,
-   *      denoted by `nfft`.
-   *      
-   *   2. The padded signal is transformed using the FFT.
+   *  1. The signal is transformed using the FFT.
+   *  
+   *  2. The first half of the frequency domain is kept (the second half is
+   *     a mirror image under complex conjugation because the signal is real).
+   *     For a given number of samples `N = x.length`, if `N` is even then 
+   *     `N/2 + 1` samples are kept, while if `N` is odd, then `(N+1)/2` are 
+   *     kept.
    *   
-   *   3. The first half of the frequency domain is kept (the second half is
-   *      a mirror image because the signal is real).
-   *      
-   *   4. The `Complex` absolute value of the frequency domain is computed.
-   *   
-   *   5. The frequency magnitudes are divided by the length of the
-   *      original signal (`x.length`).
-   *   
-   *   6. The frequency magnitudes are squared.
-   *   
-   *   7. The frequency magnitudes are multiplied by 2, except the
-   *      first (DC) and last (Nyquist).  Since only half of the original
-   *      signal was kept, this recovers the total energy.  The DC and
-   *      Nyquist frequencies are not multiplied by 2 because they are
-   *      unique.
+   *  3. The `Complex` absolute values of the frequency domain are computed.
+   *  
+   *  4. The frequency magnitudes are divided by the length of the original
+   *     signal, `N`.
+   *     
+   *  5. The frequency magnitudes are squared.
+   *  
+   *  6. The magnitudes are multiplied by 2, except the first (DC) and
+   *     Nyquist components (the Nyquist component is only present if
+   *     `N` is even).  This accounts for the complex-conjugate pairs which
+   *     were discarded in step 2.
+   * 
+   * @TODO: add windowing and spectrogram options; also compare with Matlab
+   *   output
    * 
    * @param x the signal
    * @param fs the sampling frequency (used to calculate the frequency
@@ -37,29 +39,35 @@ object PSD {
    * @return a sequence of pairs, in which the first component of the pair
    *   is the frequency band, and the second component is the power contained
    *   in that frequency band */
-  def psd(x: Seq[Double], fs: Double = 1.0): Seq[(Double, Double)] = {   
-    // pad x to the next closest power of 2
-    val nfft = FFT.nextPow2(x.length)
-    val xx = x.padTo(nfft, 0.0)
-
-    // 1. take FFT of the padded signal
-    // 2. keep only the first half (the second is symmetric)
-    // 3. take the absolute value
+  def psd(x: Seq[Double], fs: Double = 1.0): Seq[(Double, Double)] = {
+    // determine if the signal has an even number of samples
+    val isEven = (x.length % 2 == 0)
+    // compute number of unique samples in the transformed FFT
+    val nUnique = if (isEven) x.length / 2 + 1 else (x.length + 1) / 2
+    
+    // 1. take FFT
+    // 2. keep only first half
+    // 3. take the Complex absolute value
     // 4. divide by the length of the original signal
     // 5. square
-    val h1 = FFT.fft(xx).take(nfft / 2).map(_.abs).map(_ / xx.length).
+    val h1 = FFT.fft(x).take(nUnique).map(_.abs).map(_ / x.length).
       map(x => x * x)
     
-    // multiply all frequencies of the signal by 2, except the DC and
-    //  Nyquist frequencies (which are unique).  we kept only the first half 
-    //  of the signal, so this recovers the energy of the other half
-    val midFreq = h1.tail.dropRight(1).map(_ * 2.0)
-    val h = Vector(h1.head) ++ midFreq ++ Vector(h1.last)
+    // multiply frequencies by 2, except for DC and Nyquist
+    val h = if (isEven) {
+      // if there were an even number of samples, the Nyquist freq is present
+      val midFreq = h1.tail.dropRight(1).map(_ * 2.0)
+      Vector(h1.head) ++ midFreq ++ Vector(h1.last)
+    } else {
+      // for an odd number of samples, there was no Nyquist freq
+      val tailFreq = h1.tail.map(_ * 2.0)
+      Vector(h1.head) ++ tailFreq
+    }
 
-    // generate frequency vector
+    // compute vector of frequencies
     val freq = for {
-      i <- 0 until nfft/2
-      f = i.toDouble * fs / nfft
+      i <- 0 until nUnique
+      f = i.toDouble * fs / x.length.toDouble
     } yield f
 
     // return frequencies zipped with magnitudes
